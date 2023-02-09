01d5ea20b7eb2bf8f9d92d32de1ee98c972e1e09
luwasd <lmanuel14@gmail.com>
2023-02-09 00:50:03 -0300

    [TAREA] sensei

--- 7_Ninja.js
+++ 7_Ninja.js
@@ -11,12 +11,30 @@ class Ninja {
     showStats() {
         console.log("Nombre: " + this.nombre + " Fuerza: " + this.fuerza + " Velocidad: " + this.velocidad + " Salud: " + this.salud);
     }
-    drinkSake () {
+    drinkSake() {
         this.salud += 10;
     }
 }
+class Sensei extends Ninja {
+    constructor(nombre, sabiduria = 10, salud = 200, velocidad = 10, fuerza = 10) {
+        super(nombre, salud, velocidad, fuerza);
+        this.sabiduria = sabiduria;
+    }
+    showStats() {
+        console.log("Nombre: " + this.nombre + " Sabiduria: " + this.sabiduria + " Fuerza: " + this.fuerza + " Velocidad: " + this.velocidad + " Salud: " + this.salud);
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log('Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.');
+    }
+}
 let ninja1 = new Ninja('Carlos', 25);
+let superSensei = new Sensei('Splinter');
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
 ninja1.sayName();
+superSensei.sayName();
+superSensei.showStats();
+superSensei.speakWisdom();
+superSensei.showStats();
